56ccba58d71ae0de10220764f5f90f77741f86d6
AshishBilung <136777685+AshishBilung@users.noreply.github.com>
2023-07-04 20:06:52 +0530

    Add files via upload

---
+++ Laptop store Database.sql
@@ -0,0 +1,136 @@
+Create Table Laptop_comp
+(Brand varchar(50), Model Varchar(50), Market_value int, Ram varchar(20), Processor Varchar(50), Software Varchar(20), 
+Sys_storage Varchar(20),Typel varchar(20),Customer_Rating int, Quantity_sold int, Revenue int);
+
+
+Alter table laptop_comp
+modify Customer_Rating Decimal(2,1)
+
+Select*from Laptop_comp
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software, 
+Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Lenovo','Lenovo_Thinkbook','58500','8GB','Intelcore i5','Windows11_home','512GB','Chromebook','4','174','7020000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Apple','Apple_M1','81900','8GB','8_core_CPU','MacOS','256GB','Macbook air','4.5','60','4914000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Apple','Apple_M2pro','199900','8GB','12_core_CPU','MacOS','512GB','Macbook pro','4.7','40','7996000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Apple','Apple_Macbookair','107990','8GB','8_core_CPU','MacOS','256GB','Macbook air','4.4','60','6263420');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Acer','Acer_one14','34990','8GB','Ryzen_5','Windows11_home','512GB','Chromebook','4.3','160','5598400');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Lenovo','Lenovo_e41','19750','4GB','Athlon_Silver3050u','Windows10_home','256GB','Notebook','3.9','110','2172500');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Asus','Asus_Vivobook15','24990','4GB','Intel_celeron N4020','Windows11_home','256GB','Chromebook','3.9','195','4873050');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('MSI','MSI_Katana_gf66','85990','8GB','intel_core_i7','Windows10_home','512GB','Gaming','4.1','80','6879200');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Dell','Dell_15','79000','16GB','intel_core_i5','Windows10_home','512GB','Gaming','3.9','72','5688000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('HP','HP_victus','62780','8GB','intel_core_i5','Windows11_home','512GB','Gaming','4.1','109','6843020');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Acer','Acer_Extensa15','26490','4GB','intel_Pentium_silver','Windows10_home','1TB','Chromebook','3.3','50','1324500');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Asus','Asus_TUF','59990','8GB','intel_core_i5','Windows11_home','512GB','Gaming','4.3','210','12597900');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Huawei','Huawei_Matebook','100608','8GB','intel_core_i5','Windows10_home','512GB','Ultrabook','5','83','8350464');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('HP','HP_spectre','149990','16GB','intel_Evocore_i7','Windows11_home','1TB','2_in_1','3.5','17','2549830');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Lenovo','Lenovo_IdeapadGaming','55990','8GB','intel_core_i5','Windows11_home','512GB','Gaming','4.1','102','5710980');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Samsung','Sumsung_Galaxybook3','104990','16GB','intel_core_i5','Windows11_home','512GB','2_in_1','3.8','55','5774450');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('LG','LG_Gram','88990','16GB','intel_core_i7','Windows11_home','512GB','Ultrabook','4','94','8365060');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Acer','Acer_Predator','442605','64GB','intel_core_i9','Windows10_home','2TB','Gaming','4.1','9','3983445');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Asus','Asus_Rogzephyrus','385990','32GB','AMD_ryzen9','Windows11_home','2TB','Gaming','3.3','15','5789850');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Xiaomi','Xiaomi_notebookPro','53999','16GB','intel_core_i5','Windows11_home','512GB','Ultrabook','4.3','155','8369845');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Dell','Dell_Alienware','295120','32GB','intel_core_i7','Windows11_home','1TB','Gaming','5','28','8263360');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Xiaomi','Xiaomi_notebookultra','55939','16GB','intel_core_i5','Windows11_home','512GB','Ultrabook','4.2','104','5817656');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Lenovo','Lenovo_Ideaslim3','48143','8GB','intel_core_i5','Windows11_home','512GB','Ultrabook','4.2','190','9147170');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('MSI','MSI_TitanGT77','668164','64GB','intel_core_i9','Windows11_home','4TB','Gaming','2.2','5','3340820');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('HP','HP_Chromebook','21990','4GB','Mediatek_MT8183','ChromeOS','64GB','Chromebook','3.8','162','3562380');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('HP','HP_247','23243','8GB','AMD_Athlon','Windows11_home','1TB','Notebook','3.5','120','2789160');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Lenovo','Lenovo_Ideapadflex','58500','8GB','intel_core_i3','Windows10_home','256GB','2_in_1','4.2','36','2106000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Samsung','Sumsung_Galaxybook2','68300','8GB','intel_core_i5','Windows11_home','512GB','Chromebook','4','110','7513000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Samsung','Sumsung_Galaxybook3pro','134990','16GB','intel_core_i7Evo','Windows11_home','512GB','Ultrabook','3.7','65','8774350');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Acer','Acer_Aspire3','48990','8GB','intel_core_i5','Windows11_home','512GB','Ultrabook','3.7','29','1420710');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Dell','Dell_Vostro','34990','8GB','intel_core_i3','Windows11_home','256GB','Chromebook','4','205','7172950');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Dell','Dell_Inspiron','58990','8GB','intel_core_i3','Windows11_home','512GB','2_in_1','4.3','220','12977800');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('MSI','MSI_Modern15','44990','16GB','AMD_Ryzen5','Windows11_home','512GB','Chromebook','4','100','4499000');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Asus','Asus_Vivobook14','28990','8GB','intel_pentium_Silver','Windows11_home','256GB','Chromebook','3.9','155','4493450');
+insert into Laptop_comp(Brand, Model, Market_value, Ram, Processor, Software,Sys_storage,Typel,Customer_Rating, Quantity_sold, Revenue)values('Fujitsu','Fujitsu_UH-x','89990','16GB','intel_core_i7Evo','Windows11_home','512GB','Ultrabook','4.3','43','3869570');
+
+
+Create table Laptop_returned
+(Model Varchar(50), Sales_return int, Return_Value int);  
+
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Asus_Vivobook15','50','1249500');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Dell_15','15','1185000');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('HP_victus','35','2197300');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Asus_TUF','18','1079820');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Huawei_Matebook','10','1006080');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Dell_Inspiron','42','2477580');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Asus_Vivobook14','8','231920');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Acer_one14','40','1399600');
+Insert into Laptop_returned(Model,Sales_return,Return_Value)Values('Lenovo_Thinkbook','70','4095000');
+
+
+--Find Difference between Revenue and Goods Return Value
+
+With LapCTE as(
+Select Lc.Model, Market_value, Quantity_sold, Revenue, Sales_return, Return_Value
+from laptop_comp Lc
+Inner join laptop_returned Lr
+on Lc.Model=Lr.Model)
+Select*,(Revenue-Return_Value) Actual_Revenue from LapCTE;
+
+--Find Highest Revenue From Which brand?
+
+With HighCTE as (
+Select Brand,Sum(Revenue) Total_Revenue,Row_number()over(Partition by Brand order by Brand) rn from laptop_comp
+Group by Brand) 
+Select Brand, Total_Revenue from HighCTE
+order by Total_Revenue desc
+Limit 1;
+
+
+--Find Rank of a Model as per Return Value.
+
+Select Lc.Model, Return_Value, Rank()over(order by Return_Value desc) Rank_value
+from laptop_comp Lc
+Inner join laptop_returned Lr
+on Lc.Model=Lr.Model;
+
+
+--Find the Model whos Revenue is Higher then the Average Revenue of Whole Models.(Using Scalar subquery)
+
+Select * from Laptop_comp lc
+join (Select Avg(Revenue) Rev from Laptop_comp) Avg_rev
+ on lc.Revenue > Avg_rev.Rev;
+ 
+ --Find the Model who earn the highest revenue in each Typel(Using Multiple row Subquery)
+
+Select Brand,Model,Typel,Revenue from Laptop_comp
+where (Typel, Revenue) in (Select Typel, Max(Revenue)
+                           From Laptop_comp
+                           Group by Typel);
+
+--Find the Model in each Type whos revenue is more than the average Revenue in that Type.(Correlated subquery)
+
+Select*from laptop_comp lc1
+where Revenue > (Select Avg(Revenue)
+                 from  laptop_comp lc2
+                 where lc2.Typel=lc1.Typel);
+                 
+
+--Find Brand whos Market value is better than the Average Market value across all Brands.(Using Nested Subquery)
+
+Select *
+from(Select brand, Sum(Market_value) as Total_mv
+from Laptop_comp
+group by brand) Mv
+Join (Select Avg(Total_mv) Mv2
+from (
+Select brand, Sum(Market_value) as Total_mv
+from Laptop_comp
+group by brand) x) Avg_mv
+on Mv.Total_mv > Avg_mv.Mv2;
+
+--Thank You--
+
+
+
+
+
+
+
+
+
+
+
+
+
